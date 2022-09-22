5aa67da7967d669d3dc38e74d474a2dee94e530b
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2022-09-23 02:07:10 +0900

    :bug: fix: #15 CorsFilter 수정
    body: 프론트와 협업, CorsFilter 설정 수정, 클라이언트/서버 배포 후 Oauth 및 Member 테스트 결과 기존 CORS 에러 해결됨.

--- server/src/main/java/com/seollem/server/jwt/config/CorsConfig.java
+++ server/src/main/java/com/seollem/server/jwt/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*"); // addAllowedOrigin 에서 수정함
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
