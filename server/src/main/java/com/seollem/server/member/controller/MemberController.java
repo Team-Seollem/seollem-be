b063a4131e11a4d2ed684af5b076d09b61b62a2a
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2022-09-22 11:43:32 +0900

    :sparkles:  feature: #8 jwt payload decoder for EMAIL

--- server/src/main/java/com/seollem/server/member/controller/MemberController.java
+++ server/src/main/java/com/seollem/server/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.seollem.server.member.controller;
 
 import com.seollem.server.dto.SingleResponseDto;
+import com.seollem.server.jwt.decoder.TokenDecodeService;
 import com.seollem.server.member.dto.MemberDto;
 import com.seollem.server.member.entity.Member;
 import com.seollem.server.member.mapper.MemberMapper;
@@ -11,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 
 @RestController
@@ -20,18 +22,20 @@ public class MemberController {
 
     private final MemberMapper memberMapper;
     private final MemberService memberService;
+    private final TokenDecodeService tokenDecodeService;
 
-    public MemberController(MemberMapper memberMapper, MemberService memberService) {
+    public MemberController(MemberMapper memberMapper, MemberService memberService, TokenDecodeService tokenDecodeService) {
         this.memberMapper = memberMapper;
         this.memberService = memberService;
+        this.tokenDecodeService = tokenDecodeService;
     }
 
-
-    @GetMapping("/me")
-    public ResponseEntity getMember(@Valid @RequestBody MemberDto.Get requestBody) {
-        Member member = memberService.findMemberByEmail(
-                memberMapper.memberGetToMember(requestBody).getEmail()
-        );
+    @GetMapping(path = "/me")
+    public ResponseEntity getMember(@RequestHeader Map<String, Object> requestHeader) {
+        String token = requestHeader.get("authorization").toString(); // 헤더의 모든 값은 소문자로 받아진다.
+        System.out.println(token);
+        String email = tokenDecodeService.findEmail(token);
+        Member member = memberService.findMemberByEmail(email);
 
         return new ResponseEntity<>(
                 new SingleResponseDto<>(memberMapper.memberToMemberResponse(member)), HttpStatus.OK);
