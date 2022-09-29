1e2eb6332cd3d5020c1f692278fb7187b4cf3193
gyeongdeang <joohyeon0286@gmail.com>
2022-09-29 21:58:54 +0900

    :bug: 🐛 fix: 메모 수정 완료2

--- server/src/main/java/com/seollem/server/memo/MemoDto.java
+++ server/src/main/java/com/seollem/server/memo/MemoDto.java
@@ -15,32 +15,25 @@ public class MemoDto {
     public static class Post {
 
         @NotBlank(message = "메모는 공백이 아니어야 합니다.")
-        private String  memoContent;
+        private String memoContent;
 
         private int memoBookPage;
 
         private Memo.MemoType memoType;
-        //@Positive
-//        private long bookId;
-//
-//
-//        public Book getBook(){
-//            Book book = new Book();
-//            book.setBookId(bookId);
-//            return book;
-//        }
+
 
 
     }
+
     @Getter
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class Patch{
+    public static class Patch {
         private long memoId;
 
         @NotBlank(message = "메모는 공백이 아니어야 합니다.")
-        private String  memoContent;
+        private String memoContent;
 
         private Memo.MemoType memoType;
 
@@ -52,36 +45,23 @@ public class MemoDto {
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class RandomResponse{
+    public static class RandomResponse {
         private long memoId;
         private String memoContent;
         private Memo.MemoType memoType;
         private int memoBookPage;
-//        private long bookId;
-//
-//        public void setBook(Book book){
-//            this.bookId = book.getBookId();
-//        }
 
     }
-
 
 
     @Getter
     @Setter
     @AllArgsConstructor
-    public static class Response{
-      //  private long memoId;
+    public static class Response {
+        //  private long memoId;
         private Memo.MemoType memoType;
         private String memoContent;
         private int memoBookPage;
-//        private LocalDateTime createdAt;
-//        private LocalDateTime updateAt;
-//        private long bookId;
-//
-//        public void setBook(Book book){
-//            this.bookId = book.getBookId();
-//        }
 
     }
 }
