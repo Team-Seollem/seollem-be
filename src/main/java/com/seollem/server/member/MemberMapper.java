3468a26df9c26ef3bcf4998041aada95b7ab5a4c
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2023-01-10 21:08:36 +0900

    :wrench: #6 Mapstruct Unmapped Target Warning 제거

--- src/main/java/com/seollem/server/member/MemberMapper.java
+++ src/main/java/com/seollem/server/member/MemberMapper.java
@@ -1,8 +1,9 @@
 package com.seollem.server.member;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
 
   Member memberPostToMember(MemberDto.Post requestBody);
