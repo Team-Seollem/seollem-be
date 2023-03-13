dd53ab531aaea1da75dfb4403a2b9d2d35608d5f
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2023-03-13 17:30:48 +0900

    :sparkles: #4 Spring Rest Docs 기본 설정 완료 (JUnit5) -> build, bootJar 시 html 파일 생성

--- src/main/java/com/seollem/server/ServerApplication.java
+++ src/main/java/com/seollem/server/ServerApplication.java
@@ -3,11 +3,9 @@ package com.seollem.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ServerApplication {
 
   public static void main(String[] args) {
--- src/test/java/com/seollem/server/ServerApplicationTests.java
+++ src/test/java/com/seollem/server/ServerApplicationTests.java
@@ -1,93 +1,62 @@
-//package com.seollem.server;
-//
-//import org.json.JSONArray;
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.web.client.RestTemplate;
-//import org.springframework.web.util.UriComponents;
-//import org.springframework.web.util.UriComponentsBuilder;
-//
-//import java.net.URI;
-//import java.util.ArrayList;
-//
-//@SpringBootTest
-//class ServerApplicationTests {
-//
-//	@Test
-//	void contextLoads() throws JSONException {
-//		RestTemplate restTemplate = new RestTemplate();
-//
-//		UriComponents uriComponents =
-//				UriComponentsBuilder
-//						.newInstance()
-//						.scheme("http")
-//						.host("www.aladin.co.kr")
-//						.path("/ttb/api/ItemList.aspx")
-//						.queryParam("ttbkey", "ttbii123210947001")
-//						.queryParam("QueryType", "Bestseller")
-//						.queryParam("SearchTarget", "Book")
-//						.queryParam("output", "JS")
-//						.queryParam("Version", "20131101")
-//						.encode()
-//						.build();
-//		URI uri = uriComponents.toUri();
-//		String rawBestSellerDataStr = restTemplate.getForObject(uri, String.class);
-//
-//		// JSONObject 타입으로 편리하게 처리하기 위해 변환.
-//		JSONObject rawBestSellerDataJson = new JSONObject(rawBestSellerDataStr);
-//		/*
-//			{
-//				"item" :
-//				[
-//					{
-//						title,
-//						author,
-//						cover,
-//						publisher,
-//						itemPage
-//					},
-//					{
-//						title,
-//						author,
-//						cover,
-//						publisher,
-//						itemPage
-//					},
-//					...
-//					...
-//					...
-//				]
-//			}
-//
-//		 */
-//		JSONArray itemArr = rawBestSellerDataJson.getJSONArray("item");
-//
-//		ArrayList<JSONObject> booksArr = new ArrayList<>();
-//
-//		for (int i=0;i<itemArr.length();i++){
-//			booksArr.add(itemArr.getJSONObject(i));
-//		}
-//
-//		JSONArray responseBody = new JSONArray();
-//		JSONObject itemPageJson = new JSONObject();
-//		// 0,2,4,13,16
-//		for(JSONObject object : booksArr){
-//			JSONObject temp = new JSONObject();
-//			temp.put("title", object.getString("title"));
-//			temp.put("author", object.getString("author"));
-//			temp.put("cover", object.getString("cover"));
-//			temp.put("publisher", object.getString("publisher"));
-////			itemPageJson = findItemPage(object.getString("isbn"));
-//			temp.put("isbn", itemPageJson.getString("itemPage"));
-//			responseBody.put(responseBody);
-//		}
-//		System.out.println(responseBody);
-//	}
-//
-//
-//
-//
-//
-//}
+package com.seollem.server;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.seollem.server.auth.controller.AuthController;
+import com.seollem.server.member.MemberMapper;
+import com.seollem.server.member.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@WebMvcTest(AuthController.class)
+public class ServerApplicationTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private MemberService memberService;
+
+  @MockBean
+  private MemberMapper memberMapper;
+
+  @BeforeEach
+  void setUp(WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentation) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation))
+        .alwaysDo(document("{method-name}",
+            preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+        .build();
+  }
+
+  @Test
+  public void authControllerTest() throws Exception {
+
+    this.mockMvc.perform(MockMvcRequestBuilders.post("/join").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().is4xxClientError())
+        .andDo(document("index"));
+//            requestFields(fieldWithPath("email").description("User Email"),
+//                fieldWithPath("password").description("User Password"))));
+  }
+
+
+}
