a13833c69f86976c5b471ca8a75c3df8ddb837d7
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2023-03-21 21:19:28 +0900

    :sparkles: #4 Book 도메인의 'library 조회' 컨트롤러 rest docs 구현, issue:  response의 pageInfo 가 정확하지 않음.

--- src/test/java/com/seollem/server/restdocs/controller/auth/AuthControllerTests.java
+++ src/test/java/com/seollem/server/restdocs/controller/auth/AuthControllerTests.java
@@ -1,9 +1,8 @@
-package com.seollem.server.restdocs.controller;
+package com.seollem.server.restdocs.controller.auth;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
@@ -18,8 +17,8 @@ import com.seollem.server.member.Member;
 import com.seollem.server.member.MemberDto;
 import com.seollem.server.member.MemberMapper;
 import com.seollem.server.member.MemberService;
+import com.seollem.server.restdocs.util.SetUpUtil;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,32 +27,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-public class ControllerTests {
-
-  @Autowired
-  private MockMvc mockMvc;
-
-  private final String SCHEMA = "https";
-
-  private final String URI = "seollem.link";
-
-  @BeforeEach
-  void setUp(WebApplicationContext webApplicationContext,
-      RestDocumentationContextProvider restDocumentation) {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-        .apply(documentationConfiguration(restDocumentation)).alwaysDo(
-            document("{method-name}", preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()))).build();
-  }
+public class AuthControllerTests extends SetUpUtil {
 
   @Nested
   @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
@@ -70,18 +49,18 @@ public class ControllerTests {
     private Gson gson;
 
     @Test
-    public void authControllerTest() throws Exception {
+    public void authJoinTest() throws Exception {
       //given
-      MemberDto.Post post = new MemberDto.Post("khs@gmail.com", "김형섭", "password123!", "123456");
-      String content = gson.toJson(post);
-
       given(memberMapper.memberPostToMember(Mockito.any(MemberDto.Post.class))).willReturn(
           new Member());
 
       given(memberService.createMember(Mockito.any(Member.class), anyString())).willReturn(
           new Member());
 
-      //when
+      MemberDto.Post post = new MemberDto.Post("khs@gmail.com", "김형섭", "password123!", "123456");
+      String content = gson.toJson(post);
+
+      //when, then
       mockMvc.perform(MockMvcRequestBuilders.post("/join").accept(MediaType.APPLICATION_JSON)
               .contentType(MediaType.APPLICATION_JSON).content(content)).andExpect(status().isCreated())
           .andDo(document("Join",
@@ -95,10 +74,36 @@ public class ControllerTests {
     }
   }
 
-  @Nested
-  @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-  @WebMvcTest(AuthController.class)
-  class AuthLogin {
+//  @Nested
+//  @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+//  @SpringBootTest
+//  class AuthLogin {
+//
+//    @Autowired
+//    private Gson gson;
+//
+//    @Test
+//    public void authLoginTest() throws Exception {
+//      //given
+//      JsonObject jsonObject = new JsonObject();
+//      jsonObject.addProperty("email", "starrypro@gmail.com");
+//      jsonObject.addProperty("password", "Abcd123!");
+//
+//      String content = gson.toJson(jsonObject);
+//
+//      //when
+//      mockMvc.perform(MockMvcRequestBuilders.post("/login").accept(MediaType.APPLICATION_JSON)
+//              .contentType(MediaType.APPLICATION_JSON).content(content)).andExpect(status().isOk())
+//          .andDo(document("Login",
+//              preprocessRequest(modifyUris().scheme(SCHEMA).host(URI).removePort(), prettyPrint()),
+//              preprocessResponse(prettyPrint()), requestFields(
+//                  List.of(fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+//                      fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"))),
+//              responseHeaders(
+//                  headerWithName("Authorization").description("Bearer \"ACCESS TOKEN...\""),
+//                  headerWithName("Refresh").description("Bearer \"REFRESH TOKEN...\""))));
+//    }
+//  }
 
-  }
+
 }
---
+++ src/test/java/com/seollem/server/restdocs/util/SetUpUtil.java
@@ -0,0 +1,32 @@
+package com.seollem.server.restdocs.util;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public class SetUpUtil {
+
+  protected final String SCHEMA = "https";
+  protected final String URI = "seollem.link";
+  @Autowired
+  protected MockMvc mockMvc;
+
+  @BeforeEach
+  void setUp(WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentation) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation)).alwaysDo(
+            document("{method-name}", preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()))).build();
+  }
+
+}
