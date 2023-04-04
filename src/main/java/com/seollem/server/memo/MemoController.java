52315495fd314e5028adf9bcb064f6f15a49e46b
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2023-04-04 16:13:11 +0900

    :sparkles: #4 이미지 메모 등록 구현

--- src/main/java/com/seollem/server/memo/MemoController.java
+++ src/main/java/com/seollem/server/memo/MemoController.java
@@ -5,6 +5,7 @@ import com.seollem.server.book.BookService;
 import com.seollem.server.file.FileUploadService;
 import com.seollem.server.member.Member;
 import com.seollem.server.member.MemberService;
+import com.seollem.server.memo.MemoDto.ImageMemoResponse;
 import com.seollem.server.memolikes.MemoLikesService;
 import com.seollem.server.util.GetEmailFromHeaderTokenUtil;
 import java.util.Map;
@@ -69,8 +70,9 @@ public class MemoController {
     Member member = memberService.findVerifiedMemberByEmail(email);
 
     String url = fileUploadService.createImageMemo(file);
+    MemoDto.ImageMemoResponse imageMemoResponse = new ImageMemoResponse(url);
 
-    return new ResponseEntity<>(url, HttpStatus.CREATED);
+    return new ResponseEntity<>(imageMemoResponse, HttpStatus.CREATED);
   }
 
   @PatchMapping("/{memo-id}")
