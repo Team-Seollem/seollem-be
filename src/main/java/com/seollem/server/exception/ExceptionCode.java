adad087832d266e3be789ef1fb1fb704629a0b57
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2023-04-22 17:18:36 +0900

    :sparkles: #31 FileUploadService.createImage() 에서 예외 발생 시 로깅 및 BusinessLogicException 발생하도록 변경

--- src/main/java/com/seollem/server/exception/ExceptionCode.java
+++ src/main/java/com/seollem/server/exception/ExceptionCode.java
@@ -3,19 +3,18 @@ package com.seollem.server.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-  MEMBER_NOT_FOUND(404, "Member not found"),
-  MEMBER_EXISTS(400, "Member already exists"),
+  MEMBER_NOT_FOUND(404, "Member not found"), MEMBER_EXISTS(400, "Member already exists"),
   MEMBER_AUTHENTICATIONCODE_INVALID(404, "Member authentication code invalid "),
-  BOOK_NOT_FOUND(404, "Book not found"),
-  BOOK_EXISTS(400, "Book already exists"),
+  BOOK_NOT_FOUND(404, "Book not found"), BOOK_EXISTS(400, "Book already exists"),
 
   BOOK_NOT_FOUND_PERIOD(404, "Books not found in period"),
 
   BOOK_STATUS_WRONG(400, "Wrong value in bookStatus and readDate"),
-  NOT_MEMBER_BOOK(400, "Not member's book"),
-  MEMO_NOT_FOUND(404, "Memo not found"),
+  NOT_MEMBER_BOOK(400, "Not member's book"), MEMO_NOT_FOUND(404, "Memo not found"),
 
-  NOT_MEMBER_MEMO(400, "Not member's memo");
+  NOT_MEMBER_MEMO(400, "Not member's memo"),
+
+  IMAGE_UPLOAD_FAIL(500, "Image upload failed.");
 
   @Getter
   private final int status;
